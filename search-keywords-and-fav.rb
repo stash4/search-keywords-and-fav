8c3557e732f397a1e14e7ec71da30946e084686a
stash4 <high.mackey@gmail.com>
2015-08-02 06:15:29 +0900

    second

--- search-keywords-and-fav.rb
+++ search-keywords-and-fav.rb
@@ -2,10 +2,10 @@ Plugin.create :tako do
 
   on_appear do |ms|
     ms.each do |m|
-      if m.message.user.to_s =~ /Tako_white/ and
+      if m.message.user.to_s =~ /example/ and
            m.to_s =~ /キーワード|keyword/
         m.message.favorite(true)
-        Service.primary.update(:message => "@username")
+        Service.primary.update(:message => "@example")
       end
     end
   end
